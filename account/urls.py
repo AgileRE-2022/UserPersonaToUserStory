a252b639c4a4b62a33b84351afa50d5f53a05d24
Ardina DN <ardinadnn@gmail.com>
2022-05-28 10:53:37 +0700

    convert/login/welcome
    dah di-link, belum css-an, BELUM database, ada dropdown jg, histori cuman ada page TAPI kosongan

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome,name='welcome'),
+    path('home', views.welcome,name='welcome'),
     path('login', views.login,name='login'),
     path('register', views.register,name='register'),
+    path('history', views.history,name='history'),
 ]
