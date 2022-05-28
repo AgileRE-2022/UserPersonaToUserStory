39ea50b34b4785fa0cdb5484e6f6f68e2f0dec6e
Ardina DN <ardinadnn@gmail.com>
2022-05-28 22:48:25 +0700

    coba pos tagging needs (dari noun)
    tapi sangat manual, mw coba lagi

--- coba pos tagging.py
+++ coba pos tagging.py
@@ -1,24 +1,50 @@
 #coba pos tagging
-from nltk.tag import CRFTagger
-ct = CRFTagger()
-ct.set_model_file('all_indo_man_tag_corpus_model.crf.tagger')
+# from nltk.tag import CRFTagger
+# ct = CRFTagger()
+# ct.set_model_file('all_indo_man_tag_corpus_model.crf.tagger')
 
-text = "Celya pergi ke Korea untuk membeli makan."
-post_tag = ct.tag_sents([text.split()])
-post_tag = post_tag[0]
-print(post_tag)
+# text = "Celya pergi ke Korea untuk membeli makan."
+# post_tag = ct.tag_sents([text.split()])
+# post_tag = post_tag[0]
+# print(post_tag)
 
-#hasil = ct.tag_sents([['Saya','tidur','di','Rumah']])
-#print(hasil)
+# #hasil = ct.tag_sents([['Saya','tidur','di','Rumah']])
+# #print(hasil)
 
-# Perform standard imports
-import spacy
-nlp = spacy.load('en_core_web_sm')
-doc = nlp("I want to buy something but I don't have money.")
-#print(doc[4].text,"|",doc[4].pos_,"|",doc[4].tag_,"|",spacy.explain(doc[4].tag))
-                                                                 ##kata kelima
-for token in doc:
-    print(f'{token.text:{10}} {token.pos_:{8}} {token.tag_:{6}} {spacy.explain(token.tag_)}')
+# # Perform standard imports
+# import spacy
+# nlp = spacy.load('en_core_web_sm')
+# doc = nlp("I want to buy something but I don't have money.")
+# #print(doc[4].text,"|",doc[4].pos_,"|",doc[4].tag_,"|",spacy.explain(doc[4].tag))
+#                                                                  ##kata kelima
+# for token in doc:
+#     print(f'{token.text:{10}} {token.pos_:{8}} {token.tag_:{6}} {spacy.explain(token.tag_)}')
+
+import nltk
+import string
+import re #regex
+
+needs = "A site that will simplify the planning of my business trips"
+tokens = nltk.word_tokenize(needs)
+postag = nltk.pos_tag(tokens)
+gabung = " ".join([str(item) for item in postag])
+gabung = gabung.translate(str.maketrans('', '', string.punctuation))
+token2 = nltk.word_tokenize(gabung)
+#print(token2)
+if gabung.find("NN")!=0: #INI KALAU AWALANNYA NOUN
+    kalimat = gabung[:gabung.index("NN")]
+    kalimat = kalimat.split()
+    kata = kalimat[-1]
+    #print(kata) << biar tau kata apa sebelum si NN
+    # KALAU CARA INI NGEHAPUS YG AWALANNYA KAPITAL V
+    # temp = gabung.split()
+    # gabung = " ".join([ele for ele in temp if not ele[0].isupper()])
+    gabung = re.sub(r'\b[A-Z]+\b', '', gabung) #ngehapus yg semuanya caps
+    output = kata,gabung.split(kata,1)[1]
+    output =  "".join([str(item) for item in output]) #join tuple output
+    output = re.sub("\s\s+", " ", output)
+    print("I want a",output) #biar spasinya gk jelek ok
+
 """
 CC coordinating conjunction 
 CD cardinal digit 
