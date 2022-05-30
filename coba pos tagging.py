e05ede6c42364c6953ef96b33997253b64b4c438
Ardina DN <ardinadnn@gmail.com>
2022-05-30 23:54:14 +0700

    pos tagging 5/30
    needs wajib adj+noun soalnya if else gak jalan gak tau kenapa (dan V1 di awal kalimat dianggap CC sama nltk)
    error di verb yg goals gak ketangkep tapi yang verb noun ketangkep??gakjelas

--- coba pos tagging.py
+++ coba pos tagging.py
@@ -1,8 +1,7 @@
 import nltk
-import string
 
-needs = "The application to teach and find new places that I can travel to"
-goals = "I can find out about the new places I am traveling to"
+needs = "A really robust search system"
+goals = "I have to find necessary files easily"
 tokensNeeds = nltk.word_tokenize(needs)
 postagNeeds = nltk.pos_tag(tokensNeeds)
 #print(postagNeeds)
@@ -18,25 +17,44 @@ def adjNoun(teks):
             for token2 in teks:
                 if "JJ" and "NN" in token2:
                     indeks = token2.index("JJ")
+                    hasil = []
                     while(indeks<len(token2)):
-                        print(token[indeks],end=' ')
+                        b = token[indeks]
+                        hasil.append(b)
                         indeks = indeks+1
-                    return("")
+                    return(" ".join([str(item) for item in hasil]))
     except ValueError:
-        return ("")
-    
+        return("[ERROR DETECTED: Supported format = ADJECTIVE + NOUN]")
+
+def Verb(teks):
+    try:
+        for token in teks:
+            for token2 in teks:
+                if "VB" in token2:
+                    indeks = token2.index("VB")
+                    hasil = []
+                    while(indeks<len(token2)):
+                        b = token[indeks]
+                        hasil.append(b)
+                        indeks = indeks+1
+                    return(" ".join([str(item) for item in hasil]))
+    except ValueError:
+        return("[ERROR DETECTED: No verb detected]")
+
 def verbNoun(teks):
     try:
         for token in teks:
             for token2 in teks:
                 if "VBP" and "NN" in token2:
                     indeks = token2.index("VBP")
+                    hasil = []
                     while(indeks<len(token2)):
-                        print(token[indeks],end=' ')
+                        b = token[indeks]
+                        hasil.append(b)
                         indeks = indeks+1
-                    return("")
+                    return(" ".join([str(item) for item in hasil]))
     except ValueError:
-        print("")
+        return("[ERROR DETECTED: No verb-noun detected]")
 
 def Noun(teks):
     try:
@@ -44,46 +62,49 @@ def Noun(teks):
             for token2 in teks:
                 if "NN" in token2:
                     indeks = token2.index("NN")
+                    hasil = []
                     while(indeks<len(token2)):
-                        print(token[indeks],end=' ')
+                        b = token[indeks]
+                        hasil.append(b)
                         indeks = indeks+1
-                    return("")
+                    return(" ".join([str(item) for item in hasil]))
     except ValueError:
-        print("")
+        return("")
             
-def Verb(teks):
-    try:
-        for token in teks:
-            for token2 in teks:
-                if "VB" in token2:
-                    indeks = token2.index("VB")
-                    while(indeks<len(token2)):
-                        print(token[indeks],end=' ')
-                        indeks = indeks+1
-                    return("")
-    except ValueError:
-        print("Tidak ada verb")
-
-import string
-
-def is_header(line):
-    return line.startswith(string.punctuation)
 
 print("AS A USER,".center(60,"─"))
 
-# if(adjNoun(postagNeeds2)!=None or Noun(postagNeeds2)!=None):
-print("I WANT A...".center(60,"─"))
-adjNoun(postagNeeds2),print("")
-Noun(postagNeeds2),print("")
-# if(verbNoun(postagNeeds2)!=None or Verb(postagNeeds2)!=None):
-print("I WANT TO...".center(60,"─"))
-verbNoun(postagNeeds2),print("")
-Verb(postagNeeds2),print("")
+#if(adjNoun(postagNeeds2)!=None or Noun(postagNeeds2)!=None or Verb(postagNeeds2)!=None):
+if(adjNoun(postagNeeds2)!=None or Noun(postagNeeds2)!=None):
+    print("I WANT...".center(60,"─"))
+    if(adjNoun(postagNeeds2)!=None):
+        resNeeds = ["I want a",adjNoun(postagNeeds2)] #INI PRIORITAS VERB DI BAWAH
+        print("I want a",adjNoun(postagNeeds2)),print("")
+    elif(Noun(postagNeeds2)!=None):
+        res = ["I want a",Noun(postagNeeds2)]
+        print("I want a",Noun(postagNeeds2)),print("")
+    # elif(Verb(postagNeeds2)!=None):
+    #     #print("I WANT TO...".center(60,"─"))
+    #     resNeeds = ["I want to",Verb(postagNeeds2)]
+    #     print("I want to",Verb(postagNeeds2)),print("")
+    # ^^^^ IF ELSE GAK JALAN JADI NANGKAP ERROR AJA --> WAJIB ADJ+NOUN
+        
 print("SO THAT I CAN...".center(60,"─"))
-adjNoun(postagGoals2)
-verbNoun(postagGoals2)
-Noun(postagGoals2)
-Verb(postagGoals2)
+# print(adjNoun(postagGoals2))
+# print(verbNoun(postagGoals2))
+# print(Noun(postagGoals2))
+if(Verb(postagGoals2)!=None or verbNoun(postagGoals2)!=None):
+    if(verbNoun(postagGoals2)!=None): #ternyata kalau "Order a lot", order kehitung CC/konjungsi di nltk
+        resGoals = ["so that I can",verbNoun(postagGoals2)] #entah kenapa kalau error, errornya gak ketangkep
+        print(verbNoun(postagGoals2))
+    elif(Verb(postagGoals2)!=None):
+        resGoals = ["so that I can",Verb(postagGoals2)] #entah kenapa kalau error, errornya gak ketangkep
+        print(Verb(postagGoals2))
+else:
+    resGoals = ["[ERROR DETECTED: No verb detected!!]"]
+print("HASIL".center(60,"─"))
+hasil = resNeeds+resGoals
+print(" ".join([str(item) for item in hasil]))
 
 
 
