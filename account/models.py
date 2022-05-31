087e575af0814377eca9de1e5e7e26af2314cb90
Ardina DN <ardinadnn@gmail.com>
2022-05-31 13:22:59 +0700

    Revert "Merge branch 'main' of https://github.com/AgileRE-2022/UserPersonaToUserStory"
    This reverts commit 849aee08313cc2a01993bc35cddccb08017cac18, reversing
    changes made to b3c8a1f134d1388d16d031d64880ec78d5a363f1.

--- account/models.py
+++ account/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class UserPersona(models.Model) :
-    nama = models.CharField(max_length=50)
-    as_a = models.CharField(max_length=50)
-    needs = models.CharField(max_length=255)
-    goals = models.CharField(max_length=255)
-    
-    def __str__(self) :
-        return self.as_a
