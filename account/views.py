f6a2b36ba51286b5631c8fbf1c5042a673a75cc4
sal-xx <salsabilar795@gmail.com>
2022-07-06 23:38:59 +0700

    last

--- account/views.py
+++ account/views.py
@@ -50,11 +50,8 @@ def tambah_data(request) :
                 'pesan': pesan,
             }
             #return render(request, 'account/preview.html', konteks)
-<<<<<<< HEAD
             return redirect('preview')
-=======
-            return redirect('preview',preview)
->>>>>>> 0a2b39b71f19e16ab318bcfe36ec333de5fa253a
+           
     else:
         form = FormConvert()
         konteks = {
