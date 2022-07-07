2074aff5db6756573799147bc9389d311b25ce2d
nuzul <nuzulnf1712@gmail.com>
2022-07-07 08:53:45 +0700

    final form

--- account/models.py
+++ account/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class UserPersona(models.Model) :
     nama = models.CharField(max_length=50)
     as_a = models.CharField(max_length=50)
+    bio = models.TextField(max_length=1000, null=True)
+    locations = models.CharField(max_length=255, null=True)
     needs = models.TextField(max_length=1000)
     goals = models.TextField(max_length=1000)
-    locations = models.CharField(max_length=255, null=True)
-    bio = models.TextField(max_length=1000, null=True)
     frustrations = models.TextField(max_length=1000, null=True)
     behaviours = models.TextField(max_length=1000, null=True)
     date = models.DateTimeField(auto_now_add=True)
