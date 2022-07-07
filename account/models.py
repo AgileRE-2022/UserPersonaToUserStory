a34917af32839b764e443dbf79374c8ac3cae282
nuzul <nuzulnf1712@gmail.com>
2022-07-07 08:12:11 +0700

    atur posisi model

--- account/models.py
+++ account/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class UserPersona(models.Model) :
     nama = models.CharField(max_length=50)
+    as_a = models.CharField(max_length=50)
+    needs = models.TextField(max_length=1000)
+    goals = models.TextField(max_length=1000)
     locations = models.CharField(max_length=255, null=True)
     bio = models.TextField(max_length=1000, null=True)
-    frustrations = models.CharField(max_length=255, null=True)
-    behaviours = models.CharField(max_length=255, null=True)
-    as_a = models.CharField(max_length=50)
-    needs = models.CharField(max_length=255)
-    goals = models.CharField(max_length=255)
+    frustrations = models.TextField(max_length=1000, null=True)
+    behaviours = models.TextField(max_length=1000, null=True)
     date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) :
